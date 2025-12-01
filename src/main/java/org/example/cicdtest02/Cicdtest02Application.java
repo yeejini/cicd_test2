951113b8c581b0af8e71930714c7bd9f7108d24f
yeejini <ks990528@naver.com>
2025-12-01 11:54:38 +0900

    test1

---
+++ src/main/java/org/example/cicdtest02/Cicdtest02Application.java
@@ -0,0 +1,13 @@
+package org.example.cicdtest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdtest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicdtest02Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicdtest02/Cicdtest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdtest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdtest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
